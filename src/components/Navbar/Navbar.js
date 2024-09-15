3ea922bec32ec5ef1362bddae7763d9800738a01
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2024-09-14 19:40:43 -0500

    Initial commit with project structure and navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <nav className = "navbar">
+            <div className='navbar-left'>
+                <h1>Ann Hagan</h1>
+            </div> 
+            <div className='navbar-center'>
+                <ul className='navbar-links'>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <a href="https://github.com/anniepuy"><FaGithub></FaGithub></a>
+                <a href="https://www.linkedin.com/in/annmhagan/"><FaLinkedin></FaLinkedin></a>
+            </div>  
+        </nav> 
+    );
+};
+
+export default Navbar;
